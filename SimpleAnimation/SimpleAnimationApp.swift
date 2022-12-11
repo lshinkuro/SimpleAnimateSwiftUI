b11bcf9caea3387352a71f605d8ff449db46da6d
nurkholis <nur.kholis@ist.id>
2022-12-11 21:47:17 +0700

    Initial Commit

---
+++ SimpleAnimation/SimpleAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAnimationApp.swift
+//  SimpleAnimation
+//
+//  Created by nur kholis on 11/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
